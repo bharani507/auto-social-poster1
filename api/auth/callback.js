2d08660129070fff8ba91fb30cdfc25d44012bc7
bharani507 <ibharani59@gmail.com>
2026-04-24 19:32:37 +0530

    final change

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -1,34 +1,46 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  const { code } = req.query;
+    const { code } = req.query;
 
-  if (!code) {
-    return res.status(400).send("No code received");
-  }
+    if (!code) {
+        return res.status(400).send("No code received");
+    }
 
-  try {
-    // Exchange code for token
-    const tokenRes = await axios.get(
-      "https://graph.facebook.com/v19.0/oauth/access_token",
-      {
-        params: {
-          client_id: process.env.META_APP_ID,
-          client_secret: process.env.META_APP_SECRET,
-          redirect_uri: process.env.META_REDIRECT_URI,
-          code,
-        },
-      }
-    );
+    try {
+        // Exchange code for token
+        const tokenRes = await axios.get(
+            "https://graph.facebook.com/v19.0/oauth/access_token",
+            {
+                params: {
+                    client_id: process.env.META_APP_ID,
+                    client_secret: process.env.META_APP_SECRET,
+                    redirect_uri: process.env.META_REDIRECT_URI,
+                    code,
+                },
+            }
+        );
 
-    const access_token = tokenRes.data.access_token;
+        const access_token = tokenRes.data.access_token;
 
-    // Redirect to frontend with token
-    res.redirect(`/?access_token=${access_token}`);
+        // Get pages
+        const pagesRes = await axios.get(
+            "https://graph.facebook.com/me/accounts",
+            { params: { access_token } }
+        );
 
-  } catch (err) {
-    res.status(500).json({
-      error: err.response?.data || err.message,
-    });
-  }
+        const pages = pagesRes.data.data;
+
+        // Send first page (or all pages if needed)
+        const page = pages[0];
+
+        res.redirect(
+            `/?access_token=${access_token}&page_id=${page.id}`
+        );
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.response?.data || err.message,
+        });
+    }
 }
