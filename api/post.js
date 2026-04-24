96dcae753dd57f1ba6f1dda02251dcf2974c15b7
bharani507 <ibharani59@gmail.com>
2026-04-24 21:30:52 +0530

    changes small

--- api/post.js
+++ api/post.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         const page_access_token = page.access_token;
 
         let fbPost;
-        let igPost = null;
+       
 
         console.log("Incoming:", { message, image_url, video_url });
 
