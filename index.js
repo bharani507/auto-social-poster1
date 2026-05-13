959bbc5dba2145e89589f876a4da111965e1c73b
bharani507 <123951612+bharani507@users.noreply.github.com>
2026-05-13 11:53:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 
 import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
-
-dotenv.config();
-
-connectDB();
+import postRoutes from "./routes/postRoutes.js";
 
 const app = express();
+
+// 🔥 CONNECT DATABASE FIRST
+await connectDB();
 
 app.use(cors());
 
@@ -18,6 +17,8 @@ app.use(express.json());
 
 // 🔥 ROUTES
 app.use("/auth", authRoutes);
+
+app.use("/post", postRoutes);
 
 // 🔥 TEST ROUTE
 app.get("/", (req, res) => {
@@ -40,8 +41,4 @@ app.get("/", (req, res) => {
   `);
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+export default app;
